ae26fe79437648fb1d2e520faba94dc10fb89274
AfroMonkey <moisalejandro@gmail.com>
2017-05-30 17:22:23 -0500

    [REM] string='part_number'

--- models/product_template.py
+++ models/product_template.py
@@ -4,6 +4,4 @@ from openerp import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    part_number = fields.Char(
-        string='part_number',
-    )
+    part_number = fields.Char()
